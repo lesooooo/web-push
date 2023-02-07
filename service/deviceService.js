9ebb98cb05a077878ceade23236a1b1eb7dfbfe8
jspark <jspark@tnmtech.com>
2023-02-07 11:43:23 +0900

    paging 참고 코드 추가

---
+++ service/deviceService.js
@@ -0,0 +1,9 @@
+module.exports = {
+    findSiteDevices: function (param) {
+        return asyncWrapper.service(async (db) => {
+            let result = await db.execute('deviceMapper', 'findSiteDevices', param);
+            let totalCnt = await db.execute('commonMapper', 'findTotalCnt');
+            return utils.calcPaging(result, param.pageIndex, param.pageCount, totalCnt);
+        });
+    },
+};
